c101949bd3d32d259d887681dcbf03ed86ac6ef2
Mithlesh <aryanrajnwd9155@gmail.com>
2026-09-24 23:26:46 +0530

    feat: refactor agent controller for improved code readability and structure
    Signed-off-by: Mithlesh <aryanrajnwd9155@gmail.com>

--- Backend/src/controllers/agent.controllers.js
+++ Backend/src/controllers/agent.controllers.js
@@ -1,29 +1,36 @@
 import { generateresponse, generatechattitle } from "../service/service.ai.js"
-import  { chatmodel } from "../models/chat.model.js"
-import {massegemodel} from "../models/message.model.js"
+import { chatmodel } from "../models/chat.model.js"
+import { massegemodel } from "../models/message.model.js"
 
 async function agentcontroller(req, res) {
     try {
         const { message } = req.body
         const title = await generatechattitle(message)
         const result = await generateresponse(message)
+
         const chat = await chatmodel.create({
-            user:req.user.id,
+            user: req.user.id,
             title
 
+        })
+
+        const userMessage = await massegemodel.create({
+            chat: chat._id,
+            content: message,
+            role: "user"
         })
 
         const aiMessage = await massegemodel.create({
-              chat:chat._id,
-              content:result,
-              role:"ai"
+            chat: chat._id,
+            content: result,
+            role: "ai"
         })
 
         res.status(200).json({
-            aiMessage,
             chat,
-            title
-            
+            userMessage,
+            aiMessage
+
         })
     } catch (error) {
         console.error("error from " + error)
