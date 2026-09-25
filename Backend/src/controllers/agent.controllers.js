6e09726994895dc3bbcd0b1b64d7ed38028320f5
Mithlesh <aryanrajnwd9155@gmail.com>
2026-09-25 14:42:46 +0530

    feat: enhance agent controller and AI response generation with chat handling improvements
    Signed-off-by: Mithlesh <aryanrajnwd9155@gmail.com>

--- Backend/src/controllers/agent.controllers.js
+++ Backend/src/controllers/agent.controllers.js
@@ -4,36 +4,47 @@ import { massegemodel } from "../models/message.model.js"
 
 async function agentcontroller(req, res) {
     try {
-        const { message } = req.body
-        const title = await generatechattitle(message)
-        const result = await generateresponse(message)
+        const { message, chatId } = req.body
 
-        const chat = await chatmodel.create({
-            user: req.user.id,
-            title
+        let title = null, chat = null;
 
-        })
+        if (!chatId) {
+            title = await generatechattitle(message)
+            chat = await chatmodel.create({
+                user: req.user.id,
+                title
+
+            })
+        }
 
         const userMessage = await massegemodel.create({
-            chat: chat._id,
+            chat: chatId || chat._id,
             content: message,
             role: "user"
         })
 
+
+        const messages = await massegemodel.find({ chat: chatId })
+
+        const result = await generateresponse(messages)
+
+
+
         const aiMessage = await massegemodel.create({
-            chat: chat._id,
+            chat: chatId || chat._id,
             content: result,
             role: "ai"
         })
 
         res.status(200).json({
+            title,
             chat,
             userMessage,
             aiMessage
 
         })
     } catch (error) {
-        console.error("error from " + error)
+        console.error("error from ai controller" + error)
     }
 }
 
--- Backend/src/service/service.ai.js
+++ Backend/src/service/service.ai.js
@@ -4,22 +4,22 @@ dotenv.config();
 import { tool, createAgent } from "langchain";
 import * as z from "zod";
 
-import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { HumanMessage, SystemMessage, AIMessage } from "@langchain/core/messages";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 import { sendMail } from "../service/node.mailer.js";
 
 const Emailtool = tool(sendMail, {
-  name: "Emailtool",
-  description:
-    "A tool for sending emails. It takes to, html, subject and text.",
-  schema: z.object({
-    to: z.string().describe("Recipient email address"),
-    html: z.string().describe("HTML email content"),
-    subject: z.string().describe("Email subject"),
-    text: z.string().optional().describe("Plain text email content"),
-  }),
+    name: "Emailtool",
+    description:
+      "A tool for sending emails. It takes to, html, subject and text.",
+    schema: z.object({
+      to: z.string().describe("Recipient email address"),
+      html: z.string().describe("HTML email content"),
+      subject: z.string().describe("Email subject"),
+      text: z.string().optional().describe("Plain text email content"),
+    }),
 });
 
 const Mistralmodel = new ChatMistralAI({
@@ -27,29 +27,33 @@ const Mistralmodel = new ChatMistralAI({
 });
 
 const GoogleModel = new ChatGoogleGenerativeAI({
-  model: "gemini-3.5-flash-lite",
-  apiKey:process.env.GOOGLE_API_KEY
-})
+    model: "gemini-3.5-flash-lite",
+    apiKey: process.env.GOOGLE_API_KEY
+});
 
-export async function generateresponse(message) {
-  const response = await GoogleModel.invoke([
-    new HumanMessage(message)
-  ])
-  return response.text
+export async function generateresponse(messages) {
+    const response = await GoogleModel.invoke(messages.map((msg)=>{
+      if(msg.role == "user"){
+        return new HumanMessage(msg.content)
+      } else if (msg.role == "ai") {
+        return new AIMessage(msg.content)
+      }
+    }))
+    return response.text
 }
 
 export async function generatechattitle(message) {
-  const response = await Mistralmodel.invoke([
-    new SystemMessage(`you are a helpful assistant that generates concise and descriptive title for chat conversation. 
+    const response = await Mistralmodel.invoke([
+      new SystemMessage(`you are a helpful assistant that generates concise and descriptive title for chat conversation. 
         
         user will provide you with the first message of chat conversation,and you will generate a title that captures the essance the essence of the conversation in 2-4 words. the title should be clear,relevant, and enaging,giving user a quick understanding of the chat's topic
         `),
-    new HumanMessage(`Generate a title for chat conversation base on the following first message:"${message}"`)
-  ])
-  return response.text
+      new HumanMessage(`Generate a title for chat conversation base on the following first message:"${message}"`)
+    ])
+    return response.text
 }
 
-// export const agent = createAgent({
-//   model,
-//   tools: [Emailtool],
-// });
+  // export const agent = createAgent({
+  //   model,
+  //   tools: [Emailtool],
+  // });
