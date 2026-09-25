b8e405ece7a3c3ba52853a53c092547b6c2d918b
Mithlesh <aryanrajnwd9155@gmail.com>
2026-09-25 19:51:53 +0530

    feat: add chat retrieval and deletion endpoints to agent controller
    Signed-off-by: Mithlesh <aryanrajnwd9155@gmail.com>

--- Backend/src/controllers/agent.controllers.js
+++ Backend/src/controllers/agent.controllers.js
@@ -48,4 +48,78 @@ async function agentcontroller(req, res) {
     }
 }
 
+export async function getchat(req, res) {
+    try {
+        const user = req.body
+        const chat = await chatmodel.find({ user: req.user.id })
+
+        res.status(200).json({
+            message: "chat is fatched",
+            chat
+        })
+    } catch (error) {
+        console.log("error form chatgets" + error)
+    }
+
+}
+
+export async function getmessage(req, res) {
+    try {
+        const { chatId } = req.params
+        const chats = await chatmodel.find({
+            id: chatId,
+            user: req.user.id
+        })
+        if (!chats) {
+            return res.status(404).json({
+                message: "user is not found"
+            })
+        }
+
+        const message = await massegemodel.find({
+            chat: chatId
+        })
+
+        res.status(200).json({
+            message: "message is fatched",
+            chats,
+            message
+        })
+
+    } catch (error) {
+        console.error("error fom getmessage" + error)
+    }
+
+}
+
+
+
+export async function deletechat(req, res) {
+   try {
+     const  {chatId}  = req.params
+    const chat = await chatmodel.findOneAndDelete({
+        _id: chatId,
+        user: req.user.id
+    })
+    await massegemodel.deleteMany({
+        chat: chatId
+    })
+
+    if (!chat) {
+        return res.status(404).json({
+            message: "chat is not found"
+        })
+    }
+
+    res.status(200).json({
+        message:"chat is deleted"
+    })
+   } catch (error) {
+     console.error("error form deletechat" + error)
+   }
+
+}
+
+
+
 export default agentcontroller
--- Backend/src/routes.js/agent.router.js
+++ Backend/src/routes.js/agent.router.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import agents from "../controllers/agent.controllers.js"
 import { identify } from "../Middleware/atuh.middleware.js";
+import { getchat,getmessage,deletechat } from "../controllers/agent.controllers.js";
 
 export const agent = Router()
 
 agent.post("/ai",identify,agents)
+agent.get("/chat",identify,getchat)
+agent.get("/:chatId/message",identify,getmessage)
+agent.delete("/delete/:chatId/message",identify,deletechat)
